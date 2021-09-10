59475f23880f3c7aa064adbef0d18a4a1379b62c
Furkan KORKMAZGÖZ <furkan.kgz@gmail.com>
2021-09-10 20:48:43 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        // operasyonları içeren interface
+        List<Car> GetAll();
+        List<Car> GetById(int brandId);
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, BrandId = 2, ColorId = 3, DailyPrice = 20000, ModelYear = 1995, Description = "1995 Model Honda"},
+                new Car{Id = 2, BrandId = 2, ColorId = 3, DailyPrice = 60000, ModelYear = 2006, Description = "2006 Model Honda"},
+                new Car{Id = 3, BrandId = 2, ColorId = 3, DailyPrice = 90000, ModelYear = 2012, Description = "2012 Model Honda"},
+                new Car{Id = 4, BrandId = 3, ColorId = 3, DailyPrice = 75000, ModelYear = 2007, Description = "2007 Model Mazda"},
+                new Car{Id = 5, BrandId = 3, ColorId = 3, DailyPrice = 60000, ModelYear = 2006, Description = "2006 Model Mazda"},
+                new Car{Id = 6, BrandId = 3, ColorId = 3, DailyPrice = 200000, ModelYear = 2019, Description = "2019 Model Volvo"}
+            };
+        }
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+
+        public List<Car> GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            // Gönderdiğim araba id'sine sahip olan listedeki arabayı bul. 
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+
+        }
+
+        public void Delete(Car car)
+        {
+            
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+    }
+}
