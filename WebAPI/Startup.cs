1c86cab9e549e01d6a02490050a33eb3932bf026
Furkan KORKMAZGÖZ <furkan.kgz@gmail.com>
2021-11-01 03:23:58 +0300

    17. Gün Ödev

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -58,6 +58,8 @@ namespace WebAPI
             //services.AddSingleton<ICarImageService, CarImageManager>();
             //services.AddSingleton<ICarImageDal, EfCarImageDal>();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
                 options =>
@@ -89,6 +91,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
